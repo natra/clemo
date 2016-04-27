edcc7d924140ece45abf8b99f7c221d44649517d
natra <naterek@gmail.com>
2016-04-27 21:19:38 +0200

    Add validation to contact form

--- wp-content/themes/clemo/js/main.js
+++ wp-content/themes/clemo/js/main.js
@@ -31,15 +31,123 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				this.style.color = '#fff';
 			}
 		});
-		submitBtn.addEventListener("click",function(){
-			var thankyouMessage = document.querySelector('.thankyouMessage');
-			thankyouMessage.style.display = "block";
-			this.parentNode.style.display = "none";
+		var contactForm = submitBtn.parentNode;
+
+		// ==================== contact form validation =====================================
+
+		var isNameErrorVisible = false;
+		var isEmailErrorVisible = false;
+		var isMessageErrorVisible = false;
+		var isNameValid = false;
+		var isEmailValid = false;
+		var isMessageValid = false;
+		var nameInput = contactForm["name"];
+		nameInput.addEventListener("blur", function(){
+			if(this.value.length < 4){
+				this.style.borderColor = "red";
+				isNameErrorVisible = true;
+				contactForm.parentNode.querySelector(".nameLengthValidation").innerText ="Name cannot have less than 4 characters";
+			} else {
+				this.style.borderColor = "#333333";
+				isNameValid = true;
+				isNameErrorVisible = false;
+				contactForm.parentNode.querySelector(".nameLengthValidation").innerText ="";
+			}
+			(isNameErrorVisible || isEmailErrorVisible || isMessageErrorVisible) ? contactForm.style.marginTop = "20px" : contactForm.style.marginTop = "0";
+		});
+
+		var emailInput = contactForm["e-mail"];
+		emailInput.addEventListener("blur", function(){
+			var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    		var email = this.value;
+			if(this.value.length == 0){
+				this.style.borderColor = "red";	
+				
+				isEmailErrorVisible = true;
+				contactForm.parentNode.querySelector(".emailBlankValidation").innerText ="Email cannot be blank";
+			} else {
+				contactForm.parentNode.querySelector(".emailBlankValidation").innerText ="";
+				if(!re.test(email)){
+					this.style.borderColor = "red";
+					isEmailErrorVisible = true;
+					contactForm.parentNode.querySelector(".emailSyntaxValidation").innerText ="Email is invalid";
+				} else {
+					this.style.borderColor = "#333333";
+					isEmailValid = true;
+					isEmailErrorVisible = false;
+					contactForm.parentNode.querySelector(".emailBlankValidation").innerText ="";
+					contactForm.parentNode.querySelector(".emailSyntaxValidation").innerText ="";
+				}
+			}
+			(isNameErrorVisible || isEmailErrorVisible || isMessageErrorVisible) ? contactForm.style.marginTop = "20px" : contactForm.style.marginTop = "0";
+		});
+
+		var messageTextarea = contactForm["message"];
+		messageTextarea.addEventListener("blur", function(){
+			if(this.value.length < 5){
+				this.style.borderColor = "red";
+				isMessageErrorVisible = true;
+				contactForm.parentNode.querySelector(".messageLengthValidation").innerText ="Message cannot have less than 5 characters";
+			} else {
+				this.style.borderColor = "#333333";
+				isMessageValid = true;
+				isMessageErrorVisible = false;
+				contactForm.parentNode.querySelector(".messageLengthValidation").innerText ="";
+			}
+			(isNameErrorVisible || isEmailErrorVisible || isMessageErrorVisible) ? contactForm.style.marginTop = "20px" : contactForm.style.marginTop = "0";
+		});
+
+	// ========================= contact form submitting ==========
+
+		contactForm.addEventListener("submit",function(e){
+			e.preventDefault();
+			if(isNameValid && isEmailValid && isMessageValid){
+				var thankyouMessage = document.querySelector('.thankyouMessage');
+				thankyouMessage.parentNode.classList.toggle('thankyou');
+				isNameErrorVisible = false;
+				isEmailErrorVisible = false;
+				isMessageErrorVisible = false;
+				isNameValid = false;
+				isEmailValid = false;
+				isMessageValid = false;
+			} else {
+				if (!isNameValid && !isNameErrorVisible){
+					nameInput.style.borderColor = "red";
+					isNameErrorVisible = true;
+					contactForm.parentNode.querySelector(".nameLengthValidation").innerText ="Name cannot have less than 4 characters";
+				}
+				if (!isEmailValid && !isEmailErrorVisible){
+					emailInput.style.borderColor = "red";
+					isEmailErrorVisible = true;
+					contactForm.parentNode.querySelector(".emailBlankValidation").innerText ="Email cannot be blank";
+				}
+				if (!isMessageValid && !isMessageErrorVisible){
+					messageTextarea.style.borderColor = "red";
+					isMessageErrorVisible = true;
+					contactForm.parentNode.querySelector(".messageLengthValidation").innerText ="Message cannot have less than 5 characters";
+				}
+				(isNameErrorVisible || isEmailErrorVisible || isMessageErrorVisible) ? contactForm.style.marginTop = "20px" : contactForm.style.marginTop = "0";
+
+			}
 		});
 	}
+
+
+	
+	var newMessageButton = document.querySelector(".newMessage");
+	newMessageButton.addEventListener("click", function(){
+
+		var thankyouMessage = this.parentNode;
+		var contactForm = document.querySelector(".formContainer").getElementsByTagName('form')[0];
+		contactForm.reset();
+		thankyouMessage.parentNode.classList.toggle('thankyou');
+	});
+
+
 	var hamburger = document.querySelector(".hamburger");
 	hamburger.addEventListener("click",function(){
 		var header = document.querySelector(".pageHeader");
 		header.classList.toggle('navOpened');
 	});
+
 });
